40335735171d0e53a93a69c29bd50f1d9c2d758d
anas-afzal <muhammad.afzal@code.berlin>
2021-09-22 21:47:42 +0200

    chore: enable contract size optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   watcher: {
     compilation: {
       tasks: ["compile"],
