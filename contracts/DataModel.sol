ac7793da9d281acce7cb44759879659d6a062691
anas-afzal <muhammad.afzal@code.berlin>
2021-10-01 14:39:22 +0200

    refactor: break ipfs hash into Multi Hash format

--- contracts/DataModel.sol
+++ contracts/DataModel.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.0;
 library DataModel {
     enum ArtStatus{ idle, forSale, forAuction }
 
+    struct MultiHash {
+        bytes32 digest;
+        uint8 hashFunction;
+        uint8 size;
+    }
+
     struct AuctionInfo {
         uint256 currentHigh;
         address currentHighBider;
@@ -17,5 +23,6 @@ library DataModel {
         address currentOwner;
         address[] creators;
         uint256 salePrice;
+        MultiHash metaData;
     }
 }
