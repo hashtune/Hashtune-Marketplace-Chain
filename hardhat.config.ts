78b547abc7b2a19ed9a8301472156ae3fff208c2
Muhammad Anas <80168562+anas-afzal@users.noreply.github.com>
2021-10-14 18:10:43 +0200

    Feat/add gas reporter (#24)
    * refactor: override transfer NFT functions
    
    * fix: add tests to end auction before any bid
    
    * feat: add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "tsconfig-paths/register";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "hardhat-gas-reporter";
 
 // This adds support for typescript paths mappings
 
@@ -51,6 +52,9 @@ module.exports = {
     target: "ethers-v5",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
     externalArtifacts: ["externalArtifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAPAPI
   },
   defaultNetwork: "hardhat",
   networks: {
